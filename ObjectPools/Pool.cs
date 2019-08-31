c8322b4857127513b5dc778ed5853b82ac9e0986
SergioPenaloza0507 <50851215+SergioPenaloza0507@users.noreply.github.com>
2019-08-31 12:13:54 -0500

    Generic Pool Package

---
+++ ObjectPools/Pool.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+namespace ObjectPools
+{
+    public class Pool : MonoBehaviour
+    {
+        [SerializeField] GameObject[] prefabs;
+
+        [SerializeField] int maxCount;
+
+        List<List<GameObject>> objects;
+
+        bool ready = false;
+
+
+        private void Awake()
+        {
+            StartCoroutine(InitializeObjects());
+        }
+
+
+        IEnumerator InitializeObjects()
+        {
+            objects = new List<List<GameObject>>();
+            GameObject[] clones = new GameObject[prefabs.Length];
+
+            for (int i = 0; i < clones.Length; i++)
+            {
+                objects.Add(new List<GameObject>());
+                GameObject clone = Instantiate(prefabs[i], new Vector3(1000, -1000, 1000), Quaternion.identity);
+                clones[i] = clone;
+                PoolObject.AddToObject(clone);
+                objects[i].Add(clone);
+                for (int j = 0; j < maxCount - 1; j++)
+                {
+                    objects[i].Add(Instantiate(clones[i], new Vector3(1000, -1000, 1000), Quaternion.identity));
+                }
+            }
+
+            ready = true;
+            yield return null;
+        }
+
+        public GameObject GetActiveGameObject(int index)
+        {
+            if (ready)
+            {
+                try
+                {
+                    GameObject c = objects[index].First(x => !x.GetComponent<PoolObject>().Active);
+                    PoolObject p = c.GetComponent<PoolObject>();
+                    p.Activate();
+                    return c;
+                }
+                catch (Exception error)
+                {
+                    GameObject c = Instantiate(prefabs[index], new Vector3(1000, -1000, 1000), Quaternion.identity);
+                    PoolObject.AddToObject(c);
+                    c.GetComponent<PoolObject>().Activate();
+                    return c;
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ ObjectPools/PoolObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ObjectPools
+{
+    public class PoolObject : MonoBehaviour
+    {
+        bool active;
+        public static void AddToObject(GameObject g)
+        {
+            if (g.GetComponent<PoolObject>() == null)
+            {
+                g.AddComponent(typeof(PoolObject));
+            }
+        }
+
+        public void Deactivate()
+        {
+            active = false;
+            ReturnToOriginalPos();
+        }
+
+        public void Activate()
+        {
+            if (GetComponent<IPoolObject>() != null)
+            {
+                GetComponent<IPoolObject>().Activate();
+            }
+            active = true;
+        }
+
+        void ReturnToOriginalPos()
+        {
+            transform.position = new Vector3(1000, -1000, 1000);
+            print(transform.position);
+        }
+
+        public bool Active { get => active; set => active = value; }
+    }
+}
